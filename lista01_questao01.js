d835c09ccdcb3a6d77d9f349cee92d9e00040746
Neraky <felipecartaxo1@gmail.com>
2022-08-22 20:57:44 -0300

    adjusment exercise 01

--- lista01_questao01.js
+++ lista01_questao01.js
@@ -5,23 +5,20 @@ function calculadora (num1, num2, op) {
 	switch (op) {
 		case '+':
 			return num1 + num2
-			break
 		case '-':
 			return num1 - num2
-			break
 		case '*':
 			return num1 * num2
-			break
 		case '/':
 			return num1 / num2
 		default:
-			console.log('Por favor, informe uma operação válida (+, -, * ou /).')
+			console.log('Por favor, informe uma operação válida (+, -, * ou /).') // Para o caso do usuário enviar uma operação inválida
 	}
 }
 
 // Saída do programa
 console.log(calculadora(1, 1, '+')) // Teste da soma
-console.log(calculadora(4, 2, '-')) // Teste da subtração
+console.log(calculadora(7, 4, '-')) // Teste da subtração
 console.log(calculadora(3, 3, '*')) // Teste da multiplicação
 console.log(calculadora(10, 2, '/')) // Teste da divisão
 console.log(calculadora(2, 6, '!')) // Teste com uma operação inválida
